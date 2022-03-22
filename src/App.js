f743bef4432ea2523c177e8c998d8c4dc7d1fd45
Victoria <QAInstructorSky@gmail.com>
2022-03-22 14:27:24 +0000

    PriceEntryField component with simple label

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import PriceEntryField from './PriceEntryField';
 
 function App() {
   return (
     <div className="App">
       VAT CALCULATOR
+      <PriceEntryField label="Price excl VAT: " />
+      <PriceEntryField label="Price incl VAT: " />
     </div>
   );
 }
---
+++ src/PriceEntryField.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PriceEntryField = (props) => {
+    return (
+        <div>{props.label}</div>
+    )
+}
+
+export default PriceEntryField
