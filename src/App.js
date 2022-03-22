889f897682e786b0fef1fd6a02b506a203e448fa
Victoria <QAInstructorSky@gmail.com>
2022-03-22 15:39:28 +0000

    netPrice and grossPrice state created with state value passed as prop

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
+import { useState } from 'react';
 import './App.css';
 import DisplayBlock from './DisplayBlock';
 import PriceEntryField from './PriceEntryField';
 import VatRateField from './VatRateField';
 
 function App() {
+  const [netPrice, setNetPrice] = useState(0.0);
+  const [grossPrice, setGrossPrice] = useState(0.0);
   return (
     <div className='header field'>
       VAT CALCULATOR
       <div className='pale-green-border'>
         <VatRateField customstyle="field" />
-        <PriceEntryField customstyle="field" label="Price excl VAT: " price={20} />
+        <PriceEntryField customstyle="field" label="Price excl VAT: " price={netPrice === 0.0 ? "" : netPrice} />
         <DisplayBlock customstyle="field" label="VAT to pay: " value="4" />
-        <PriceEntryField customstyle="field" label="Price incl VAT: " price={24} />
+        <PriceEntryField customstyle="field" label="Price incl VAT: " price={grossPrice === 0.0 ? "" : grossPrice} />
       </div>
     </div>
   );
